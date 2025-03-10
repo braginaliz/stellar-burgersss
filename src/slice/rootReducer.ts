acb5a0ceb4fa11271ab98033c9dbd3cb59bcebf8
braginaliz <158730026+braginaliz@users.noreply.github.com>
2025-03-10 18:27:46 +0300

    Update rootReducer.ts

--- src/slice/rootReducer.ts
+++ src/slice/rootReducer.ts
@@ -1,9 +1,9 @@
-import { combineReducers } from 'redux';
+import { combineSlices } from 'redux';
 import userReducer from './userSlice';
 import ingredientsReducer from './ingredientsSlice';
 import ordersReducer from './ordersSlice';
 
-const rootReducer = combineReducers({
+const rootReducer = combineSlices({
   user: userReducer,
   ingredients: ingredientsReducer,
   orders: ordersReducer,
